940d61cbff5fd33408ffdf39bb4ac25274d763c2
vitthalk123 <vitthalk564@gmail.com>
2024-07-30 22:47:07 -0700

    updating c-codes

---
+++ even_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+int num;
+printf("enter a number");
+scanf("%d",&num);
+if(num/2==0)
+{
+printf("condition is odd");
+}
+else {
+printf("condition is even");
+}
+}
